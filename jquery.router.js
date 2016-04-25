f86306452967813bd4daafdbb6b9d02f082f0084
jgmdev <jgmdev@gmail.com>
2016-04-25 00:31:40 -0400

    Some additions and improvements by mergin most of the pull requests on original repository.

--- jquery.router.js
+++ jquery.router.js
@@ -1,15 +1,10 @@
 /*
-
-    plugin name: router
-    jquery plugin to handle routes with both hash and push state
-    why? why another routing plugin? because i couldnt find one that handles both hash and pushstate
-    created by 24hr // camilo.tapia
-    author twitter: camilo.tapia
-  
     Copyright 2011  camilo tapia // 24hr (email : camilo.tapia@gmail.com)
+    Copyright 2015 Christoph Obexer <cobexer@gmail.com>
+    Copyright 2016 Jefferson Gonzalez <jgmdev@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
+    it under the terms of the GNU General Public License, version 2, as
     published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
@@ -20,337 +15,285 @@
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+/* jshint bitwise: true, curly: true, eqeqeq: true, forin: true, freeze: true, latedef: true,
+ maxerr: 1000, noarg: true, undef:true, unused: true, browser: true, jquery: true, laxcomma: true */
 
-*/
- 
+(function($) {
+	var router, routeList, routesOptimized, currentUsedUrl, $router, root;
 
-(function($){
-    
-    var hasPushState = (history && history.pushState);    
-    var hasHashState = !hasPushState && ("onhashchange" in window) && false;
-    var router = {};
-    var routeList = [];
-    var eventAdded = false;
-    var currentUsedUrl = location.href; //used for ie to hold the current url
-    var firstRoute = true;
-    
-    // hold the latest route that was activated
-    router.currentId = "";
-    router.currentParameters = {};
-    
-    router.capabilities = {
-        hash: hasHashState,
-        pushState: hasPushState,
-        timer: !hasHashState && !hasPushState
-    };
-    
-    // reset all routes
-    router.reset = function()
-    {
-        var router = {};
-        var routeList = [];
-        router.currentId = "";
-        router.currentParameters = {};
+	router = {};
+	routeList = [];
+	routesOptimized = true;
+	$router = $(router);
+
+	// hold the latest route that was activated
+	router.currentId = "";
+	router.currentParameters = {};
+
+	function stripSlash(url) {
+		var u = url.replace(/[/]+/g, '/'); // normalize multiple consecutive slashes to a single slash
+		if ('/' === u.charAt(u.length - 1)) {
+			u = u.substring(0, u.length - 1);
+		}
+		return u;
+	}
+
+	function stripRoot(url) {
+		var result = root ? false : url;
+		if (root && 0 === url.indexOf(root)) {
+			result = url.substring(root.length);
+		}
+		return result;
+	}
+
+	root = stripSlash(location.pathname);
+
+	// reset all routes
+	router.reset = function() {
+		routeList = [];
+		routesOptimized = true;
+		router.currentId = "";
+		router.currentParameters = {};
+	};
+
+	function RegexRoute(route) {
+		this._regex = route;
+	}
+
+	RegexRoute.prototype.match = function(url) {
+		return url.match(this._regex);
+	};
+
+	RegexRoute.prototype.weight = function() {
+		return [];
+	};
+
+	function StringRoute(route) {
+		var spec = stripSlash(route), weight = [];
+		// if the routes where created with an absolute url, we have to remove the absolute part anyway, since we can't change that much
+		spec = spec.replace(location.protocol + "//", "").replace(location.hostname, "");
+		this._parts = spec.split('/').map(function(value) {
+			var result = { parameter: false, str: value };
+			if (':' === value.charAt(0)) {
+				result.parameter = true;
+				result.str = value.substring(1);
+				weight.push(0);
+			}
+			else {
+				weight.push(1);
+			}
+			return result;
+		});
+		this._weight = weight;
+	}
+
+	StringRoute.prototype.match = function(url) {
+		var currentUrlParts, matches = false, data = {};
+		currentUrlParts = url.split("/");
+
+		// first check so that they have the same amount of elements at least
+		if (this._parts.length === currentUrlParts.length) {
+			matches = this._parts.every(function(part, i) {
+				if (part.parameter) {
+					data[part.str] = decodeURI(currentUrlParts[i]);
+				}
+				else if (currentUrlParts[i] !== part.str) {
+					return false;
+				}
+				return true;
+			});
+		}
+		return matches ? data : false;
+	};
+
+	StringRoute.prototype.weight = function() {
+		return this._weight;
+	};
+
+	router.chroot = function(newRoot) {
+		root = stripSlash(newRoot);
+	};
+
+	router.add = function(route, id, callback) {
+		var routeItem;
+		routesOptimized = false;
+		// if we only get a route and a callback, we switch the arguments
+		if (typeof id === "function") {
+			callback = id;
+			id = null;
+		}
+
+		if (typeof route === "object") {
+			routeItem = new RegexRoute(route);
+		}
+		else {
+			routeItem = new StringRoute(route);
+		}
+
+		routeItem.id = id;
+		routeItem.callback = callback;
+		routeList.push(routeItem);
+	};
+
+	router.go = function(url, title, trigger) {
+		if (history.pushState) {
+			history.pushState({}, title, root + url + location.search);
+		}
+
+        if(typeof trigger != "boolean" || trigger !== false){
+            checkRoutes(url);
+        }
+	};
+
+    // check if a given URL is defined in router
+    router.checkURL = function(url) {
+        var parser, path, path_ltrim, path_rtrim;
+
+        if(typeof url == "object"){
+            parser = url;
+        }
+        else {
+            parser = document.createElement('a');
+            parser.href = url;
+        }
+
+        path = parser.pathname + parser.hash;
+        path_ltrim = path.replace(/^\//, "");
+        path_rtrim = path_ltrim.replace(/\/$/, "");
+
+        if(window.location.origin == parser.origin || parser.origin == ""){
+            if(checkRoutes(path_rtrim, false)){
+                return true;
+            } else if(checkRoutes(path_ltrim, false)){
+                return true;
+            }
+
+            return checkRoutes(path);
+        }
+
+        return false;
     }
- 
-    router.add = function(route, id, callback)
-    {
-        // if we only get a route and a callback, we switch the arguments
-        if (typeof id == "function")
-        {
-            callback = id;
-            delete id;
-        }
-        
-        var isRegExp = typeof route == "object";
-        
-        if (!isRegExp)
-        {
-            
-            // remove the last slash to unifiy all routes
-            if (route.lastIndexOf("/") == route.length - 1)
-            {
-                route = route.substring(0, route.length - 1);
-            }
-            
-            // if the routes where created with an absolute url ,we have to remove the absolut part anyway, since we cant change that much
-            route = route.replace(location.protocol + "//", "").replace(location.hostname, "");
+
+	// do a check without affecting the history
+	router.check = router.redo = function() {
+		// if the history api is available use the real current url; else use the remembered last used url
+		var url = history.pushState ? stripRoot(location.pathname) : currentUsedUrl;
+		checkRoutes(url);
+	};
+
+	function matchRoute(url) {
+		var match = false;
+		routeList.every(function(route) {
+			var data = route.match(url);
+			if (data) {
+				match = {
+					route: route,
+					data: data
+				};
+				// break after first hit
+				return false;
+			}
+			return true;
+		});
+		return match;
+	}
+
+	function optimizeRoutes() {
+		routeList.sort(function(a, b) {
+			var i, l, wa, wb, ia, ib;
+			wa = a.weight();
+			wb = b.weight();
+			l = Math.max(wa.length, wb.length);
+			for (i = 0; i < l; ++i) {
+				ia = wa.length > i ? wa[i] : -1;
+				ib = wb.length > i ? wb[i] : -1;
+				if (ia > ib) {
+					return -1;
+				}
+				else if (ia < ib) {
+					return 1;
+				}
+			}
+			return 0;
+		});
+		routesOptimized = true;
+	}
+
+	function checkRoutes(url, trigger404) {
+		var currentUrl, match;
+
+		if (!routesOptimized) {
+			optimizeRoutes();
+		}
+		currentUrl = stripSlash(decodeURI(url));
+		match = matchRoute(currentUrl);
+
+		if (match) {
+			currentUsedUrl = url;
+			router.currentId = match.route.id;
+			router.currentParameters = match.data;
+			match.route.callback(router.currentParameters);
+
+            return true;
+		}
+		else if(typeof trigger404 != "boolean" || trigger404 !== false) {
+			$router.triggerHandler('route404', [ url ]);
+		}
+
+        return false;
+	}
+
+	function handleRoutes(e) {
+		if (e && e.originalEvent && e.originalEvent.state !== undefined) {
+			$.router.checkURL(window.location);
+		}
+	}
+
+	router.on = function() {
+		return $router.on.apply($router, arguments);
+	};
+
+	router.off = function() {
+		return $router.off.apply($router, arguments);
+	};
+
+	router.init = function(argName) {
+		var args, prefix;
+		args = location.search.substr(1).split('&');
+		prefix = argName + '=';
+		args.every(function(arg, idx) {
+			var matches = prefix === arg.substring(0, prefix.length);
+			if (matches) {
+				args.splice(idx, 1);
+				history.replaceState({}, document.title, stripSlash(root + '/' + arg.substring(prefix.length)) + (args.length ? ('?' + args.join('&')) : ''));
+				router.check();
+			}
+			return !matches;
+		});
+	};
+
+    //binds 'a' elements to router with optional jQuery selector
+    router.bind = function(selector){
+        if(typeof selector == "undefined"){
+            selector = 'a';
         }
 
-        var routeItem = {
-            route: route,
-            callback: callback,
-            type: isRegExp ? "regexp" : "string",
-            id: id
-        }
-
-        routeList.push(routeItem);
-        
-        // we add the event listener after the first route is added so that we dont need to listen to events in vain
-        if (!eventAdded)
-        {
-            bindStateEvents();
-        }
-    };
-    
-    function bindStateEvents()
-    {
-        eventAdded = true;
-        
-        // default value telling router that we havent replaced the url from a hash. yet.
-        router.fromHash = false;
-
-        
-        if (hasPushState)
-        {
-            // if we get a request with a qualified hash (ie it begins with #!)
-            if (location.hash.indexOf("#!/") === 0)
-            {
-                // replace the state
-                var url = location.pathname + location.hash.replace(/^#!\//gi, "");
-                history.replaceState({}, "", url);
-                
-                // this flag tells router that the url was converted from hash to popstate
-                router.fromHash = true;
-            }
-            
-            $(window).bind("popstate", handleRoutes);
-        }
-        else if (hasHashState)
-        {
-            $(window).bind("hashchange.router", handleRoutes);
-        }
-        else
-        {
-            // if no events are available we use a timer to check periodically for changes in the url
-            setInterval(
-                function()
-                {
-                    if (location.href != currentUsedUrl)
-                    {
-                        handleRoutes();
-                        currentUsedUrl = location.href;
+        $(selector).each(function(index, element){
+            if($(element).is('a')){
+                $(element).click(function(e) {
+                    var url;
+                    url = $(this).attr('href');
+                    if ($.router.checkURL(url)) {
+                        $.router.go(url, null, false);
+                        e.preventDefault();
+                        return false;
                     }
-                }, 500
-            );
-        }
-       
-    }
-    
-    bindStateEvents();
-    
-    router.go = function(url, title)
-    {   
-        if (hasPushState)
-        {
-            history.pushState({}, title, url);
-            checkRoutes();
-        }
-        else
-        {
-            // remove part of url that we dont use
-            url = url.replace(location.protocol + "//", "").replace(location.hostname, "");
-            var hash = url.replace(location.pathname, "");
-            
-            if (hash.indexOf("!") < 0)
-            {
-                hash = "!/" + hash;
+                });
             }
-            location.hash = hash;
-        }
+        });
     };
-    
-    // do a check without affecting the history
-    router.check = router.redo = function()
-    {   
-        checkRoutes(true);
-    };
-    
-    // parse and wash the url to process
-    function parseUrl(url)
-    {
-        var currentUrl = url ? url : location.pathname;
-        
-        currentUrl = decodeURI(currentUrl);
-        
-        // if no pushstate is availabe we have to use the hash
-        if (!hasPushState)
-        {   
-            if (location.hash.indexOf("#!/") === 0)
-            {
-                currentUrl += location.hash.substring(3);
-            }
-            else
-            {
-                return '';
-            }
-        }
-        
-        // and if the last character is a slash, we just remove it
-        currentUrl = currentUrl.replace(/\/$/, "");
 
-        return currentUrl;
-    }
-    
-    // get the current parameters for either a specified url or the current one if parameters is ommited
-    router.parameters = function(url)
-    {
-        // parse the url so that we handle a unified url
-        var currentUrl = parseUrl(url);
-        
-        // get the list of actions for the current url
-        var list = getParameters(currentUrl);
-        
-        // if the list is empty, return an empty object
-        if (list.length == 0)
-        {
-            router.currentParameters = {};
-        }
-        
-        // if we got results, return the first one. at least for now
-        else 
-        {
-            router.currentParameters = list[0].data;
-        }
-        
-        return router.currentParameters;
-    }
-    
-    function getParameters(url)
-    {
-
-        var dataList = [];
-        
-       // console.log("ROUTES:");
-
-        for(var i = 0, ii = routeList.length; i < ii; i++)
-        {
-            var route = routeList[i];
-            
-            // check for mathing reg exp
-            if (route.type == "regexp")
-            {
-                var result = url.match(route.route);
-                if (result)
-                {
-                    var data = {};
-                    data.matches = result;
-                    
-                    dataList.push(
-                        {
-                            route: route,
-                            data: data
-                        }
-                    );
-                    
-                    // saves the current route id
-                    router.currentId = route.id;
-                    
-                    // break after first hit
-                    break;
-                }
-            }
-            
-            // check for mathing string routes
-            else
-            {
-                var currentUrlParts = url.split("/");
-                var routeParts = route.route.split("/");
-                
-                //console.log("matchCounter ", matchCounter, url, route.route)
-
-                // first check so that they have the same amount of elements at least
-                if (routeParts.length == currentUrlParts.length)
-                {
-                    var data = {};
-                    var matched = true;
-                    var matchCounter = 0;
-
-                    for(var j = 0, jj = routeParts.length; j < jj; j++)
-                    {
-                        var isParam = routeParts[j].indexOf(":") === 0;
-                        if (isParam)
-                        {
-                            data[routeParts[j].substring(1)] = decodeURI(currentUrlParts[j]);
-                            matchCounter++;
-                        }
-                        else
-                        {
-                            if (routeParts[j] == currentUrlParts[j])
-                            {
-                                matchCounter++;
-                            }
-                        }
-                    }
-
-                    // break after first hit
-                    if (routeParts.length == matchCounter)
-                    {
-                        dataList.push(
-                            {
-                                route: route,
-                                data: data
-                            }
-                        );
-
-                        // saved the current route id
-                        router.currentId = route.id;
-                        router.currentParameters = data;
-                        
-                        break; 
-                    }
-                    
-                }
-            }
-            
-        }
-        
-        return dataList;
-    }
-    
-    function checkRoutes()
-    {
-        var currentUrl = parseUrl(location.pathname);
-
-        // check if something is catched
-        var actionList = getParameters(currentUrl);
-        
-        // ietrate trough result (but it will only kick in one)
-        for(var i = 0, ii = actionList.length; i < ii; i++)
-        {
-            actionList[i].route.callback(actionList[i].data);
-        }
-    }
-    
-
-    function handleRoutes(e)
-    {
-        if (e != null && e.originalEvent && e.originalEvent.state !== undefined)
-        {
-            checkRoutes();
-        }
-        else if (hasHashState)
-        {
-            checkRoutes();
-        }
-        else if (!hasHashState && !hasPushState)
-        {
-            checkRoutes();
-        }
-    }
-
-
-
-    if (!$.router)
-    {
-        $.router = router;
-    }
-    else
-    {
-        if (window.console && window.console.warn)
-        {
-            console.warn("jQuery.status already defined. Something is using the same name.");
-        }
-    }
-        
-})( jQuery );
+	$(window).bind("popstate", handleRoutes);
+	$.router = router;
+})(jQuery);
--- readme.md
+++ readme.md
@@ -1,69 +1,129 @@
 # jquery router plugin
-This plugin handles routes for both hash and push state.
 
-Why? Why another routing plugin? Because i couldn't find one that handles both hash and pushstate.
+This plugin handles routes for push state with a simple api and
+deterministic route ordering.
+
+### Setup
+
+Setting up your application for use of this plugin is very easy:
+
+1) Include the this plugin.
+2) Optionally call $.router.init with the name of your url argument that
+contains the initial route that should be displayed.
+3) Profit!
 
 ### How to add routes
-
-To add route you simply call the add function, providing it with the actual route string, an optional id, and the callback. 
+To add route you simply call the add function, providing it with the
+actual route string, an optional id, and the callback.
 
 $.router.add(*route*, *[id]*, *callback*);
-	
+
 Example:
 
-	// Adds a route for /items/:item and calls the callback when matched
-	$.router.add(/items/:item", function(data) {
-		console.log(data.item);
-	});
+```js
+// Adds a route for /items/:item and calls the callback when matched
+$.router.add("/items/:item", function(data) {
+    console.log(data.item);
+});
+```
 
 or
 
-	// Adds a route for /items/:item and calls the callback when matched, but also has
-	// a reference to the routes id in $.router.currentId
-	$.router.add(/items/:item", "foo", function(data) {
-		console.log(data.item);
-	});
+```js
+// Adds a route for /items/:item and calls the callback when matched, but also has
+// a reference to the routes id in $.router.currentId
+$.router.add("/items/:item", "foo", function(data) {
+    console.log(data.item);
+});
+```
 
 ### How to change urls and trigger routes
 You can also change the current url to a route, and thereby triggering the route by calling *go*.
 
+```js
 $.router.go(url, title);
+```
 
 Example:
-	
-	// This will change the url to http://www.foo.com/items/mycoolitem and set the title to
-	// "My cool item" without reloading the page. If using hashes instead, it will use the url
-	// http://www.foo.com/#!items/mycoolitem.
-	// If a route has been set that matches it, it will be triggered.
-	$.router.go("/items/mycoolitem", "My cool item");
-	
-### Reseting all routes
 
+    // This will change the url to http://www.foo.com/items/42 and set the title to
+    // "My cool item" without reloading the page.
+    // If a route has been set that matches it, it will be triggered.
+    $.router.go("/items/42", "My cool item");
+
+Routes are examined from most the specific route to least specific route:
+
+    Consider the following routes as added (in any order):
+    /\/items\/(\d+)/: a regular expression route
+    "/:p1/:p2" a route with 2 parameter
+    "/:type/edit" a route with a parameter as first argument
+    "/items/:item": a simple route with a parameter
+    "/items/all": a simple route without any parameter
+    The order in which the routes will be checked is:
+    /items/all          checked first because it has 2 static parts
+    /items/:item        checked second because it has 1 static part
+    /:type/edit         checked after the route above because of the position of the parameter
+    /:p1/:p2            checked because it has 2 parameters (more than those above)
+    /\/items\/(\d+)/    regular expression routes are checked last
+
+Please note that the ordering between regular expression based routes is undefined.
+
+__For more usage examples check the included tests inside the test directory.__
+
+### Reseting all routes
 If you need to remove all routes (which is good when testing) you just call:
 
 $.router.reset();
 
-## License 
+### Events
+In case a route attempt leads to no match the ___route404___ event gets triggered with the url as argument:
+to ease event handling $.router.on and $.router.off are aliases to jQuerys on and off functions.
 
+```js
+// register an event handler to respond to route misses.
+$.router.on('route404', function(e, url) {
+    console.log('No route for ' + url);
+});
+// unregister all event handlers again.
+$.router.on('route404');
+```
+
+### Handling browser reloads and incoming deep links
+
+Enabling deep links and fully supporting browser history within your
+application is hopefully one of the key reasons you selected this library.
+Both features require server side support, your server needs to be able to
+respond to URL's that don't actually exist on the server, in a way that
+your application can understand and turn into the correct content on the
+client side.
+
+Suppose you have a ToDo application and the URL looks like
+this: https://example.org/my-todos/
+(this is where your application,jQuery, and this plugin reside).
+A user clicks on an a link on an external site like:
+
+    https://example.org/my-todos/v/list/kNKHVXol/all
+
+and to enable the deep linking your server redirects that to:
+
+    https://example.org/my-todos/?v=/v/list/kNKHVXol/all
+
+To handle this URL with jquery.router.js call ```$.router.init('v')``` v
+being the name of the URL argument that contains the URL to route to.
+The result will look the same in the client after your application has
+initialized, but all your assets can be loaded using relative
+URLs which is a nice bonus for applications that are also installed in
+an on-premises context where absolute control over URL setup is not possible.
+
+If you have no idea how to get your server to send redirects like that
+and you also use Apache HTTPD check out the .htaccess file inside the test directory
+because it does exactly that :).
+
+In case you only deploy your application as a service or you have another
+way of handling nonexistent URLs with your web-server of choice you may
+need to change the root of what **$.router** is going to handle, to do that
+call ```$.router.chroot(root)``` with the path that marks the root of
+what **$.router** should handle / modify.
+
+## License
 (The MIT License)
-
-Copyright (c) 2011 Camilo Tapia &lt;camilo.tapia@gmail.com&gt;
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-'Software'), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
